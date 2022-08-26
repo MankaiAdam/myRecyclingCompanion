c3365d4852642226bf37f1d7ea10dcefb305666f
MankaiAdam <Adouma2003>
2022-08-26 19:44:00 +0100

    test

--- app/src/main/java/com/example/myrecyclingcompanion/HomeActivity.java
+++ app/src/main/java/com/example/myrecyclingcompanion/HomeActivity.java
@@ -64,7 +64,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }).show();*/
             Log.d("tag",result.getContents());
-            //test
         }
     });
 }
